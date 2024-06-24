753d1ae9c938ff1adba22d67dd4765895c50acdd
DeltaNeverUsed <sinus06@hotmail.com>
2024-06-25 00:25:44 +0200

    3.25GH/s

--- main.cu
+++ main.cu
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <cuda/std/bit>
 #include <chrono>
+#include <thread>
 
-__device__ constexpr uint64_t hashes_per_thread = 0x010000;
+__device__ constexpr uint64_t hashes_per_thread = 0x001000;
 __device__ constexpr char* chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 __device__ constexpr unsigned int message_prefix_len = 16;
 
-__device__ const uint32_t K[64] = { 
+__device__ constexpr uint32_t K[64] = { 
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -18,27 +19,27 @@ __device__ const uint32_t K[64] = {
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
  };
 
-__device__ uint32_t Ch(uint32_t x, uint32_t y, uint32_t z) {
+__device__ inline uint32_t Ch(uint32_t x, uint32_t y, uint32_t z) {
     return (x & y) ^ ((~x) & z);
 }
 
-__device__ uint32_t Maj(uint32_t x, uint32_t y, uint32_t z) {
+__device__ inline uint32_t Maj(uint32_t x, uint32_t y, uint32_t z) {
     return (x & y) ^ (x & z) ^ (y & z);
 }
 
-__device__ uint32_t Sigma0(uint32_t x) {
+__device__ inline uint32_t Sigma0(uint32_t x) {
     return cuda::std::rotr(x, 2) ^ cuda::std::rotr(x, 13) ^ cuda::std::rotr(x, 22);
 }
 
-__device__ uint32_t Sigma1(uint32_t x) {
+__device__ inline uint32_t Sigma1(uint32_t x) {
     return cuda::std::rotr(x, 6) ^ cuda::std::rotr(x, 11) ^ cuda::std::rotr(x, 25);
 }
 
-__device__ uint32_t sigma0(uint32_t x) {
+__device__ inline uint32_t sigma0(uint32_t x) {
     return cuda::std::rotr(x, 7) ^ cuda::std::rotr(x, 18) ^ (x >> 3);
 }
 
-__device__ uint32_t sigma1(uint32_t x) {
+__device__ inline uint32_t sigma1(uint32_t x) {
     return cuda::std::rotr(x, 17) ^ cuda::std::rotr(x, 19) ^ (x >> 10);
 }
 
@@ -82,6 +83,7 @@ __device__ __host__ void print_uint32(uint32_t value) {
 }
 
 __device__ __host__ bool is_hash_smaller(const uint32_t* hash1, const uint32_t* hash2) {
+    #pragma unroll
     for (size_t i = 0; i < 8; ++i) {
         if (hash1[i] < hash2[i]) {
             return true;
@@ -98,15 +100,33 @@ __global__ void gpu_hash(uint64_t nonce_start, uint32_t *hashes, uint64_t *nonce
         uint32_t ints[16];
     } prefix;
 
+    auto thread_id = threadIdx.x;
+    auto block_id = blockIdx.x;
+    auto global_id = thread_id + block_id * blockDim.x;
+
+    auto hash_index = global_id * 8;
+
+    hashes[hash_index] = 0xFFFFFFFF;
+    hashes[hash_index + 1] = 0xFFFFFFFF;
+    hashes[hash_index + 2] = 0xFFFFFFFF;
+    hashes[hash_index + 3] = 0xFFFFFFFF;
+    hashes[hash_index + 4] = 0xFFFFFFFF;
+    hashes[hash_index + 5] = 0xFFFFFFFF;
+    hashes[hash_index + 6] = 0xFFFFFFFF;
+    hashes[hash_index + 7] = 0xFFFFFFFF;
+
+    uint64_t nonce_p1 = (global_id + nonce_start) * hashes_per_thread;
+    constexpr auto m_len = message_prefix_len + 16;
+
+    auto flipped_len = __byte_perm((m_len-1) * 8, 0, 0x0123);
+
     for (size_t itter = 0; itter < hashes_per_thread; itter++)
     {
-        auto thread_id = threadIdx.x;
-        auto block_id = blockIdx.x;
-        auto global_id = thread_id + block_id * blockDim.x;
-        uint64_t nonce = (global_id + nonce_start) * hashes_per_thread + itter;
+        
+        uint64_t nonce = nonce_p1 + itter;
         uint64_t n = nonce;
 
-        auto m_len = message_prefix_len + 16;
+        #pragma unroll
         for (size_t i = message_prefix_len-1; i < m_len-1; i++)
         {
             prefix.bytes[i] = chars[nonce % 62];
@@ -115,24 +135,23 @@ __global__ void gpu_hash(uint64_t nonce_start, uint32_t *hashes, uint64_t *nonce
                 nonce = 0;
         }
 
-        prefix.ints[15] = __byte_perm((m_len-1) * 8, 0, 0x0123);
+        prefix.ints[15] = flipped_len;
         
-
         union {
             uint8_t  message[512];
             uint32_t words[64];
         };
 
-
         //message[message_prefix_len-1] = 0x80;
 
         // maybe fix bit swappy swap
+        #pragma unroll
         for (size_t i = 0; i < 64; i+=4)
         {
+            #pragma unroll
             for (size_t l = 0; l < 4; l++)
             {
-                auto index = i + 3 - l;
-                message[i + l] = prefix.bytes[index];
+                message[i + l] = prefix.bytes[i + 3 - l];
             }
 
             //printf("%u\n", words[i/4]);
@@ -144,9 +163,7 @@ __global__ void gpu_hash(uint64_t nonce_start, uint32_t *hashes, uint64_t *nonce
         #pragma unroll
         for (uint16_t i = 16; i < 64; i++)
         {
-            auto womp = sigma1(words[i-2]) + words[i-7];
-            auto wamp = sigma0(words[i-15]) + words[i-16];
-            words[i] = womp + wamp;
+            words[i] = sigma1(words[i-2]) + words[i-7] + sigma0(words[i-15]) + words[i-16];
             //printf("%u, %u\n", i, words[i]);
         }
 
@@ -161,66 +178,66 @@ __global__ void gpu_hash(uint64_t nonce_start, uint32_t *hashes, uint64_t *nonce
     
         constexpr uint32_t h0[8] = { 0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19 };
 
-        uint32_t a = 0x6a09e667;
-        uint32_t b = 0xbb67ae85;
-        uint32_t c = 0x3c6ef372;
-        uint32_t d = 0xa54ff53a;
-        uint32_t e = 0x510e527f;
-        uint32_t f = 0x9b05688c;
-        uint32_t g = 0x1f83d9ab;
-        uint32_t h = 0x5be0cd19;
+        union {
+        struct {
+            uint32_t a;
+            uint32_t b;
+            uint32_t c;
+            uint32_t d;
+            uint32_t e;
+            uint32_t f;
+            uint32_t g;
+            uint32_t h;
+        };
+        uint32_t arr[8];
+    } u = {0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19};
+
         
         #pragma unroll
         for (size_t i = 0; i < 64; i++)
         {
-            uint32_t t1 = h + Sigma1(e) + Ch(e,f,g) + K[i] + words[i];
-            uint32_t t2 = Sigma0(a) + Maj(a,b,c);
+            uint32_t t1 = u.h + Sigma1(u.e) + Ch(u.e,u.f,u.g) + K[i] + words[i];
+            uint32_t t2 = Sigma0(u.a) + Maj(u.a,u.b,u.c);
 
-            h = g;
-            g = f;
-            f = e;
-            e = d + t1;
-            d = c;
-            c = b;
-            b = a;
-            a = t1 + t2;
+            u.h = u.g;
+            u.g = u.f;
+            u.f = u.e;
+            u.e = u.d + t1;
+            u.d = u.c;
+            u.c = u.b;
+            u.b = u.a;
+            u.a = t1 + t2;
         }
 
-        auto index = global_id * 8;
-
-        if (itter == 0) {
-            hashes[index] = a + h0[0]; //__byte_perm(a + h0[0], 0, 0x0123);
-            hashes[index + 1] = b + h0[1]; //__byte_perm(b + h0[1], 0, 0x0123);
-            hashes[index + 2] = c + h0[2]; //__byte_perm(c + h0[2], 0, 0x0123);
-            hashes[index + 3] = d + h0[3]; //__byte_perm(d + h0[3], 0, 0x0123);
-            hashes[index + 4] = e + h0[4]; //__byte_perm(e + h0[4], 0, 0x0123);
-            hashes[index + 5] = f + h0[5]; //__byte_perm(f + h0[5], 0, 0x0123);
-            hashes[index + 6] = g + h0[6]; //__byte_perm(g + h0[6], 0, 0x0123);
-            hashes[index + 7] = h + h0[7]; //__byte_perm(h + h0[7], 0, 0x0123);
-            nonces[global_id] = n;
-        } else {
-            uint32_t hash[8];
-            hash[0] = a + h0[0];
-            hash[1] = b + h0[1];
-            hash[2] = c + h0[2];
-            hash[3] = d + h0[3];
-            hash[4] = e + h0[4];
-            hash[5] = f + h0[5];
-            hash[6] = g + h0[6];
-            hash[7] = h + h0[7];
-
-            if (is_hash_smaller(hash, hashes + index)) {
-                hashes[index] = hash[0];
-                hashes[index + 1] = hash[1];
-                hashes[index + 2] = hash[2];
-                hashes[index + 3] = hash[3];
-                hashes[index + 4] = hash[4];
-                hashes[index + 5] = hash[5];
-                hashes[index + 6] = hash[6];
-                hashes[index + 7] = hash[7];
-                nonces[global_id] = n;
+        auto new_better = false;
+        size_t i = 0;
+        #pragma unroll
+        for (;i < 4; ++i) {
+            u.arr[i] = u.arr[i] + h0[i];
+            if (u.arr[i] < hashes[hash_index + i]) {
+                new_better = true;
+                break;
+            } else {
+                break;
             }
         }
+        i++;
+        if (new_better){
+            #pragma unroll
+            for (size_t j = 0; j < i; j++)
+            {
+                hashes[hash_index + j] = u.arr[j];
+            }
+
+            #pragma unroll
+            for (size_t j = i; j < 8; j++)
+            {
+                hashes[hash_index + j] = u.arr[j] + h0[j];
+            }
+            nonces[global_id] = n;
+        }
+        
+        
     }
     
     //prefix.bytes[m_len-1] = 0;
@@ -243,13 +260,25 @@ void get_print_hash(uint64_t nonce) {
     printf(bytes);
 }
 
+uint64_t hashes_done;
+
+void hashrate_check() {
+    while (true)
+    {
+        _sleep(15000);
+        
+        printf("\nHashrate: %fGH/s\n", hashes_done / 1000000000. / 15);
+        hashes_done = 0;
+    }
+}
+
 int main() {
 
     uint32_t current_best_hash[8];
     for (size_t i = 0; i < 8; i++)
         current_best_hash[i] = 0xFFFFFFFF;
 
-    uint64_t nonce_start = 1053060396;
+    uint64_t nonce_start = 17063690622567168097;
 
     auto grid_dim = 38 * 2;
     auto block_dim = 256;
@@ -264,11 +293,10 @@ int main() {
 
     cudaMalloc(&device_hashes, arr_size_bytes);
     cudaMalloc(&device_nonces, arr_size * sizeof(uint64_t));
-
-    uint64_t hashes_done = 0;
-    auto hash_start = std::chrono::high_resolution_clock::now();
     
     gpu_hash<<<grid_dim, block_dim>>>(nonce_start, device_hashes, device_nonces);
+    std::thread check = std::thread(hashrate_check);
+
     while (true)
     {
         cudaDeviceSynchronize();
@@ -276,12 +304,6 @@ int main() {
         cudaMemcpy(nonces, device_nonces, arr_size * sizeof(uint64_t), cudaMemcpyDeviceToHost);
         gpu_hash<<<grid_dim, block_dim>>>(nonce_start + arr_size * hashes_per_thread, device_hashes, device_nonces);
         hashes_done += arr_size * hashes_per_thread;
-
-        if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now()-hash_start) >= std::chrono::seconds(10)) {
-            hash_start += std::chrono::seconds(10);
-            printf("\nHashrate: %fGH/s\n", hashes_done / 1000000000. / 10);
-            hashes_done = 0;
-        }
         
         for (size_t i = 0; i < arr_size; i++)
         {
@@ -306,12 +328,12 @@ int main() {
                 
                 for (size_t j = 0; j < 64; j++){
                     if (temp[j+1] != '0') {
-                        printf("Got %u zeros\n\n", j);
+                        printf("Got %llu zeros\n\n", j);
                         break;
                     }
                 }
 
-                printf("%u\n", nonces[i]);
+                printf("%llu\n", nonces[i]);
                 
             }
         }
